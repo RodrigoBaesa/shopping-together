402ae96efd18d6b439bbaf1a1fb55f91b8cff331
Rodrigo <rodrigobaesa@gmail.com>
2026-03-24 22:53:37 -0300

    build: setting up SQL

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, session
+from flask import Flask, render_template, session
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
