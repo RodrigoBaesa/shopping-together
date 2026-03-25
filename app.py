333e7e0ca81c2e813ca67bc87b53657164ba7285
Rodrigo <rodrigobaesa@gmail.com>
2026-03-25 12:55:31 -0300

    feat: implementing register route and user table

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -15,9 +18,20 @@ Session(app)
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    hash = db.Column(db.Text, nullable=False)
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
 if __name__ == '__main__':
     with app.app_context():
