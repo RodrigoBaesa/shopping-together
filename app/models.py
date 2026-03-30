0b39b27ff79f39b3139b7900e87e9116b092c774
Rodrigo <rodrigobaesa@gmail.com>
2026-03-30 18:39:48 -0300

    feat: implementing lists model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,10 @@ class User(db.Model):
     username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     hash = db.Column(db.Text, nullable=False)
+
+class List(db.Model):
+    __tablename__ = 'lists'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
